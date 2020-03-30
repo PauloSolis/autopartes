9e57502a0042c540e6dcdcb9303aa467928be9b2
Paulo <a01207593@itesm.mx>
2020-03-29 21:35:13 -0600

    fix date display

--- autopartes/users/forms.py
+++ autopartes/users/forms.py
@@ -12,7 +12,7 @@ class UserRegister(UserCreationForm):
     ruc = forms.IntegerField(min_value=00000000000, max_value=99999999999, label='RUC ',
                              widget=forms.TextInput(attrs={'placeholder': 'ej. 98688242308'}))
     email = forms.EmailField(label='Correo electrónico', widget=forms.EmailInput(attrs={'placeholder': 'ejemplo@gmail.com'}))
-    birthday = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget())
+    birthday = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=range(1960, 2012), attrs={'class': 'form-control snps-inline-select'}))
     phone = PhoneNumberField(label='Teléfono', widget=forms.TextInput(attrs={'placeholder': 'ej. +524617857592'}))
     mobile = PhoneNumberField(label='Teléfono celular', widget=forms.TextInput(attrs={'placeholder': 'ej. +524617857592'}))
     address = forms.CharField(label='Dirección', widget=forms.TextInput(attrs={'placeholder': 'Calle, colonia y número'}))
@@ -30,11 +30,12 @@ class UserRegister(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
-        #self.fields['birthday'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['ruc'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['address'].widget.attrs['class'] = 'form-control'
         self.fields['city'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['phone'].widget.attrs['class'] = 'form-control'
         self.fields['mobile'].widget.attrs['class'] = 'form-control'
