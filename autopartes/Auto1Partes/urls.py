6f50c16b80d2bbafc5ad05c0fa6b80f16cadb018
Rodolfo Rincon <a01206310@itesm.mx>
2020-04-01 04:26:38 -0600

    preping catalog

--- autopartes/Auto1Partes/urls.py
+++ autopartes/Auto1Partes/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('productos/', include('productos.urls')),
+    path('dashboards/', include('dashboard.urls')),
 
 ]
--- autopartes/dashboard/views.py
+++ autopartes/dashboard/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.db import DatabaseError
+from django.utils import timezone
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+
+def ver_catalogo(request):
+    return render(request, '../templates/store.html')
