6c29dcfda6979532b17cdb331e1086f8048efdcd
Paulo <a01207593@itesm.mx>
2020-04-10 18:02:10 -0500

    modificar rutas

--- autopartes/users/forms.py
+++ autopartes/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms import DateInput
 from phonenumber_field.formfields import PhoneNumberField
 from .models import User
@@ -11,11 +11,16 @@ class UserRegister(UserCreationForm):
     last_name = forms.CharField(label='Apellidos ')
     ruc = forms.IntegerField(min_value=00000000000, max_value=99999999999, label='RUC ',
                              widget=forms.TextInput(attrs={'placeholder': 'ej. 98688242308'}))
-    email = forms.EmailField(label='Correo electrónico', widget=forms.EmailInput(attrs={'placeholder': 'ejemplo@gmail.com'}))
-    birthday = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=range(1960, 2012), attrs={'class': 'form-control snps-inline-select'}))
+    email = forms.EmailField(label='Correo electrónico',
+                             widget=forms.EmailInput(attrs={'placeholder': 'ejemplo@gmail.com'}))
+    birthday = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=range(1960, 2012),
+                                                                                          attrs={
+                                                                                              'class': 'form-control snps-inline-select'}))
     phone = PhoneNumberField(label='Teléfono', widget=forms.TextInput(attrs={'placeholder': 'ej. +524617857592'}))
-    mobile = PhoneNumberField(label='Teléfono celular', widget=forms.TextInput(attrs={'placeholder': 'ej. +524617857592'}))
-    address = forms.CharField(label='Dirección', widget=forms.TextInput(attrs={'placeholder': 'Calle, colonia y número'}))
+    mobile = PhoneNumberField(label='Teléfono celular',
+                              widget=forms.TextInput(attrs={'placeholder': 'ej. +524617857592'}))
+    address = forms.CharField(label='Dirección',
+                              widget=forms.TextInput(attrs={'placeholder': 'Calle, colonia y número'}))
     city = forms.CharField(label='Ciudad')
 
     class Meta:
@@ -24,7 +29,6 @@ class UserRegister(UserCreationForm):
             'first_name', 'last_name', 'username', 'ruc', 'email', 'password1', 'password2', 'address', 'city',
             'birthday',
             'phone', 'mobile')
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -39,3 +43,17 @@ class UserRegister(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['phone'].widget.attrs['class'] = 'form-control'
         self.fields['mobile'].widget.attrs['class'] = 'form-control'
+
+
+class AuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Email')
+    password = forms.CharField(label='Contraseña')
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['email'].widget.attrs['class'] = 'form-control'
+            self.fields['password'].widget.attrs['class'] = 'form-control'
+
--- autopartes/users/urls.py
+++ autopartes/users/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
+
+
 
 app_name = 'users'
 
-
 urlpatterns = [
-    path('crear/', views.RegisterView.as_view(), name='crear'),
+    path('signup/', views.RegisterView.as_view(), name='signup'),
     path('ver/', views.displayUsers, name='ver'),
+    path('ver/rol/<int:id>/', views.changeRole, name='rol'),
     path('', views.HomeView.as_view(), name='home'),
-
-
-    ]
+    path(r'^logout/$', LogoutView.as_view(), name='logout'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+]
