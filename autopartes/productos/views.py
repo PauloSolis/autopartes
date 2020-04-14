ebed669aa9d04cce4399bb3182b37eb5fce0f5e1
Ana Lucía Dueñas Chávez <a01702080@itesm.mx>
2020-04-14 15:19:02 -0500

    Migrations

--- autopartes/productos/views.py
+++ autopartes/productos/views.py
@@ -1,53 +1,39 @@
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
-from .models import Product
-from .forms import Products
 from django.db import DatabaseError
-from django.contrib.auth.decorators import login_required
-from users.decorators import admin_required, retailer_required, wholesaler_required
-from django.utils.decorators import method_decorator
-from django.utils import timezone
-from django.views.generic.edit import UpdateView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from users.decorators import admin_required
+from .forms import Products
+from .models import Product
 
 STATUS_SAVED = 'SAVED'
 STATUS_ERROR = 'ERROR'
 
+
 @admin_required
 def crear_producto(request):
     if request.method == 'POST':
-        form = Products(request.POST)
-        context = {
-            'form': form,
-        }
+        form = Products(request.POST, request.FILES)
+
         if form.is_valid():
-            try:
-                new_product = Product(
-                    original_code=form.cleaned_data.get('original_code'),
-                    product_code=form.cleaned_data.get('product_code'),
-                    name=form.cleaned_data.get('name'),
-                    description=form.cleaned_data.get('description'),
-                    car_brand=form.cleaned_data.get('car_brand'),
-                    car_model=form.cleaned_data.get('car_model'),
-                    car_year=form.cleaned_data.get('car_year'),
-                    public_price=form.cleaned_data.get('public_price'),
-                    card_price=form.cleaned_data.get('card_price'),
-                    master_price=form.cleaned_data.get('master_price'),
-                    wholesale_price=form.cleaned_data.get('wholesale_price'),
-                    dozen_price=form.cleaned_data.get('dozen_price'),
-                )
-                new_product.save()
-                messages.success(request, 'Se guardo correctamente el nuevo producto')
-                return redirect('productos:ver_producto')
-            except DatabaseError:
-                messages.error(request, 'Error')
-                return render(request, '../templates/productos/crear_producto.html')
+            print("entre al is valid")
+            form.save()
+            messages.success(request, 'Se guardo correctamente el nuevo producto')
+            return HttpResponse("/productos/ver/")
+        else:
+            form.save()
+            return redirect("productos:ver_producto")
     else:
         form = Products()
         context = {
             'form': form,
         }
         return render(request, '../templates/productos/crear_producto.html', context)
+
+
+
 
 @admin_required
 def ver_producto(request):
@@ -58,6 +44,7 @@ def ver_producto(request):
     page_obj = paginator.get_page(page_number)
 
     return render(request, '../templates/productos/ver_producto.html', {'products': page_obj})
+
 
 @admin_required
 def delete_product(request, id):
