6b916ac156f0308bdf0ce272c280e75c08834b06
Ana Lucía Dueñas Chávez <a01702080@itesm.mx>
2020-04-24 15:06:52 -0500

    Front-end dashboard

--- autopartes/Auto1Partes/settings.py
+++ autopartes/Auto1Partes/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'auto1partes',
         'USER': 'postgres',
-        'PASSWORD': 'PauloSolis1',
+        'PASSWORD': '1234567890',
         'HOST': 'localhost',
         'PORT': '',
     }
