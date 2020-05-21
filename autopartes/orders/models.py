004e49e9227790b548df5552478fa625e32e9dc7
Rodolfo Rincon <a01206310@itesm.mx>
2020-05-21 16:06:30 -0500

    crear ordenes

--- autopartes/orders/models.py
+++ autopartes/orders/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from productos.models import Product
 import productos
 
-# Create your models here.
+
 class Order(models.Model):
     total_price = models.FloatField(null=False)
     status = models.TextField(null=False)
-    products = models.ManyToManyField(productos)
+    products = models.ManyToManyField(Product)
     #address = models.ForeignKey(addresses)
+
+class ProductsOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False)
+    price = models.FloatField(null=False)
--- autopartes/orders/views.py
+++ autopartes/orders/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.paginator import Paginator
+from productos.models import Product
+from orders.models import ProductsOrder
+from orders.models import Order
+import json
+import logging
 
 # Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def crear_orden(request):
+    if request.method == 'POST':
+        received_order = json.loads(request.body)
+        data = json.loads(received_order['data'][0])
+        logger.error(data['total'])
+        order = Order(total_price=data['total'][0], status="pendiente")
+        order.save()
+        for product in received_order['products']:
+            prod_decoded = json.loads(product)
+            aux_product = Product.objects.get(pk=prod_decoded['prod_id'])
+            prod_ord = ProductsOrder(
+                order_id=order,
+                product_id=aux_product,
+                quantity=prod_decoded['prod_quantity'],
+                price=prod_decoded['price_at_sale'])
+            prod_ord.save()
+    return render(request, '../templates/landing.html')
+
+
+def ver_ordenes(request):
+    orders = Order.objects.all().order_by('-id')
+    paginator = Paginator(orders, 20)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, '../templates/orders/ver_ordenes.html', {'orders': page_obj})
+
+def ver_desgloce(request, id):
+    order = ProductsOrder.objects.get(order_id=id)
+    logger.error(order[0])
+    prod = []
+    for o in order:
+        aux = Product.objects.get(id = o.product_id)
+        prod.append(aux)
+
+    return render(request, '../templates/orders/ver_desgloce.html', {'order': order, 'products':prod})
